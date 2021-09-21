26d5f35fdc16d04bd9ab171711a3d3b2e1902aa3
mgritsch04 <Gritsch.Maximilian@sus.htl-grieskirchen.at>
2021-09-21 14:37:56 +0200

    2. commit

---
+++ src/net/htlgkr/gritschm190062/hue1/PrimeSieve.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.htlgkr.gritschm190062.hue1;
+
+/**
+ *
+ * @author maxim
+ */
+public interface PrimeSieve {
+
+    public boolean isPrime(int p);
+
+    public void printPrimes();
+}
